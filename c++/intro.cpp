b79349305ec3643b17b02462061352895b6edd0e
Moon2340 <farjanamoon20144@gmail.com>
2022-12-01 21:39:25 +0600

    sum using cpp

--- c++/intro.cpp
+++ c++/intro.cpp
@@ -19,3 +19,24 @@ int main()
     getch();
 }
 */
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a number: ";
+    cin>> num;
+    cout<<"Enterd number is: "<<num;
+    return 0;
+}*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, sum;
+    cout<<"Enter Two Numbers:\n";
+    cin>> num1 >> num2;
+    sum= num1+num2;
+    cout<< "The sum is: "<<sum;
+    return 0;
+}
