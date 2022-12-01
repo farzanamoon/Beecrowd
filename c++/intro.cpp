4140afdd27e59e88e3d706d4622c24ec23c083cb
Moon2340 <farjanamoon20144@gmail.com>
2022-12-01 11:58:27 +0600

    Created-New-Folder

---
+++ c++/intro.cpp
@@ -0,0 +1,17 @@
+/*#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    cout << "Farzana Moon";
+    getch ();
+}
+*/
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    cout<< "\tFarzana"<< endl<<"moon";
+    getch();
+}
