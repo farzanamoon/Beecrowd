f61b6aed1f1f74803c0ee27a3956ba1d30c2c77e
Moon2340 <farjanamoon20144@gmail.com>
2022-12-13 03:55:14 +0600

    solution of beecrowd problem

---
+++ c/1040 Average 3.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    double N1, N2, N3,N4,N5,avg;
+    scanf("%lf %lf %lf %lf %lf",&N1,&N2,&N3,&N4,&N5);
+    avg=(N1*2+N2*3+N3*4+N4*1)/10;
+    printf("Media: %.1lf\n", avg);
+    if(avg>7.0)
+    {
+        printf("Aluno aprovado");
+    }
+    else if(avg>=5.0 && avg<=6.9)
+    {
+        printf("Aluno em exame. \n");
+        printf("Nota do exame: %.1lf\n", N5);
+        avg= (avg+N5)/2;
+        if (avg>=5.0)
+            {
+            printf("Aluno aprovado. \n");
+        }
+
+        else{
+            printf("Aluno reprovado");
+        }
+         printf("Media final: %.1lf \n", avg);
+    }
+    else
+    {
+        printf("Aluno reprovado");
+    }
+
+}
+
