e2eab99ffc9f0fb249979beab50614ee41edd50f
Moon2340 <farjanamoon20144@gmail.com>
2022-12-13 04:01:02 +0600

    little changes

--- c/1040 Average 3.c
+++ c/1040 Average 3.c
@@ -3,7 +3,7 @@ int main()
 {
     double N1, N2, N3,N4,N5,avg;
     scanf("%lf %lf %lf %lf %lf",&N1,&N2,&N3,&N4,&N5);
-    avg=(N1*2+N2*3+N3*4+N4*1)/10;
+    avg=((N1*2+N2*3+N3*4+N4*1)/10);
     printf("Media: %.1lf\n", avg);
     if(avg>7.0)
     {
@@ -15,19 +15,20 @@ int main()
         printf("Nota do exame: %.1lf\n", N5);
         avg= (avg+N5)/2;
         if (avg>=5.0)
-            {
-            printf("Aluno aprovado. \n");
+        {
+            printf("Aluno aprovado.\n");
         }
 
-        else{
-            printf("Aluno reprovado");
+        else
+        {
+            printf("Aluno reprovado\n");
         }
          printf("Media final: %.1lf \n", avg);
     }
     else
     {
-        printf("Aluno reprovado");
+        printf("Aluno reprovado\n");
     }
-
+return 0;
 }
 
