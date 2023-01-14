e4175f23ce50d7f2b8a4455b386a13278ea181e0
Moon2340 <farjanamoon20144@gmail.com>
2023-01-14 21:56:08 +0600

    solution

---
+++ c++/1065- Even Between five Numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,count=0;
+    for(i=1; i<=5; i++)
+    {
+        cin>>n;
+        if (n%2==0)
+            count++ ;
+
+    }
+    cout<<count<< "valores pares"<<endl;
+    return 0;
+
+
+
+}
+
