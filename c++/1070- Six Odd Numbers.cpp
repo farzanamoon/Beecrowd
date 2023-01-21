803055eaab5d19bd1d903e27d52d0a0ab1e1cc61
Moon2340 <farjanamoon20144@gmail.com>
2023-01-22 02:15:56 +0600

    solution

---
+++ c++/1070- Six Odd Numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num,i=0;
+    cin>>num;
+  while(i<6)
+   {
+       if(num%2!=0)
+       {
+           cout<<num<<endl;
+           i++;
+       }
+   num++;
+   }
+    return 0;
+}
